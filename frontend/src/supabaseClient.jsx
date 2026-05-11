9511f14f783e2e0a17d056a5e1401671e710a3d1
Akwasi <122885868+apoa5@users.noreply.github.com>
2026-05-11 23:54:39 +0000

    updated .env

--- frontend/src/supabaseClient.jsx
+++ frontend/src/supabaseClient.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://fqsdiegeymnfctiiuplw.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxc2RpZWdleW1uZmN0aWl1cGx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MjgxMzAsImV4cCI6MjA5NDEwNDEzMH0.G4XZjiIqepgjrdZyA4LuTk-41scKhlOBCqjlb0rnzEk";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
