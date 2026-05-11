8f68bd87b1bb5ad770afae3bc47dbb6b913a8900
Akwasi <122885868+apoa5@users.noreply.github.com>
2026-05-11 22:59:24 +0000

    Updated supabase tokens

--- frontend/src/supabaseClient.jsx
+++ frontend/src/supabaseClient.jsx
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://wryvahtsqmejklygnywt.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndyeXZhaHRzcW1lamtseWdueXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5NjU0NjYsImV4cCI6MjA3MDU0MTQ2Nn0.6lOPOe3-biS3MhnzXkAkQUylfd8AczFXvwMzO92YkS8";
+const SUPABASE_URL = "https://fqsdiegeymnfctiiuplw.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxc2RpZWdleW1uZmN0aWl1cGx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1MjgxMzAsImV4cCI6MjA5NDEwNDEzMH0.G4XZjiIqepgjrdZyA4LuTk-41scKhlOBCqjlb0rnzEk";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
